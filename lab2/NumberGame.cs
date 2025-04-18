29c3a9dc34d9ef9a293da38155dbc44691fc5aed
Gyan Hari Dahal <dgyanhari@gmail.com>
2025-04-18 22:24:24 +0545

    Lab2

---
+++ lab2/NumberGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class NumberGame
+    {
+       public int SumOfNumber(int n)
+        {
+            int sum = 0;
+            int total = n;
+            while (n != 0)
+            {
+                sum = sum + total;
+                total--;
+            }
+            return sum;
+        }
+
+
+        public int factorial(int total)
+        {
+            int factorial = 1;
+            while (total != 0)
+            {
+                factorial = factorial * total;
+                total--;
+            }
+            return factorial;
+        }
+
+
+        public bool CheckPrime(int n)
+        {
+            int demominator= n;
+            int no_divider = 0;
+            if (n <= 1) { return false; }
+
+            while (demominator != 0)
+            {
+
+                if(n%demominator == 0)
+                {
+                    no_divider++;
+                }
+                 demominator--;
+            }
+            return (no_divider>2) ?  false: true;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,59 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a;
+        Console.WriteLine("Enter Value of a");
+        a = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter Value of b");
+        //b = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter Value of b");
+        //c = Convert.ToInt32(Console.ReadLine());
+        //if (a > b && a > c)
+        //{
+        //    Console.WriteLine("THe Largest Number is: a = " + a);
+        //}
+        //else if (b > a && b > c)
+        //{
+        //    Console.WriteLine("THe Largest Number is: b = " + b);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("THe Largest Number is: c = " + c);
+
+        //}
+
+        //if(a%2 == 0 && a>0)
+        //{
+        //    Console.WriteLine("The number is even");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("The number is odd");
+        //}
+
+        //if (a%4 ==0)
+        //{
+        //    Console.WriteLine("THe Year is a leap year");
+        //}
+        //int sum = 0;
+        //int[] arr = { 1, 2, 3, 4, 5 };
+        //foreach (int i in arr)
+        //{
+        //    sum += i;
+        //}
+        //Console.WriteLine(sum);
+
+        //NumberGame fact = new NumberGame();
+        //int v = fact.factorial(a);
+        //Console.WriteLine("The factorial is: " + v);
+
+
+        NumberGame prime = new NumberGame();
+        bool v = prime.CheckPrime(a);
+        string message = v ? "The Given Number "+ a +" is prime" : "The Given Number "+ a +" is Not Prime";
+        Console.WriteLine(message);
+        }
+    }
